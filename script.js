51bab2a37ea60043b014f0e89bdb82f7a862872e
Bharathkumar1522 <bk152002@gmail.com>
2025-02-01 14:18:39 +0530

    Add animations

--- script.js
+++ script.js
@@ -14,4 +14,37 @@ window.addEventListener('scroll', () => {
     hill4.style.left = value * -1.5 + 'px';
     hill1.style.top = value * 1 + 'px';
 
-})
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.querySelector('.matter-container');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('float');
+                observer.unobserve(entry.target); // Stop observing once the animation is triggered
+            }
+        });
+    }, {
+        threshold: 0.5 // Trigger the animation when 10% of the element is visible
+    });
+
+    observer.observe(element);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.photo-collection');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('float-photos');
+                observer.unobserve(entry.target); // Stop observing once the animation is triggered
+            }
+        });
+    }, {
+        threshold: 0.3 // Trigger the animation when 10% of the element is visible
+    });
+    elements.forEach((element) => {
+        observer.observe(element);
+    })
+});
